67859061b7ab54937f528dc474b259a2c25b017b
caseylholston <caseylholston@gmail.com>
2017-02-28 15:43:29 -0500

    Need to figure out how to get the selected value from the form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,25 @@
 $(document).ready(function(){
 
 //  Set our timer counter to 20.
-var countdownTimer = 20;
+var countdownTimer = 100;
 //  Variable that will hold our interval ID when we execute the "run" function
 var intervalId;
+
+var content = { questions: {one:['Who shot John F. Kennedy?'],
+							two:['What is 2+2'],
+							},
+				possibleAnswers: { one:['<input type ="radio" name="answer1a" value="Rafael Cruz" id="1a">Rafael Cruz', '<input type ="radio"  name="answer1b" value="The CIA" id ="1b">The CIA', 
+										'<input type ="radio" name="answer1c" value="Lee Harvey Oswald" id ="1c">Lee Harvey Oswald', '<input type ="radio" name="answer1d" value="Jack Ruby" id ="1d">Jack Ruby'],
+						   		   two:['<input type ="radio" value="1" id="2a">1','<input type ="radio" value="1" id="2b">2',
+						   		   		'<input type ="radio" value="3" id="2c">3','<input type ="radio" value="4" id="2d">4'],
+								} ,
+				correctAnswers: {one:'1c',
+								 two: '2d',
+								},
+				playerAnswers: {one:'',
+							   two:'',
+							  },
+				};
 
 var triviaStatus = {startTimer:function run() {
       								intervalId = setInterval(this.decreaseTimer, 1000);
@@ -15,13 +31,13 @@ var triviaStatus = {startTimer:function run() {
 								      //  Decrease number by one.
 								      countdownTimer--;
 								      //  Show the number in the #show-number tag.
-								      $("#timer").html("<h2>" + countdownTimer + "</h2>");
+								      $('#timer').html('<h2>' + countdownTimer + '</h2>');
 								      //  Once number hits zero...
 								      if (countdownTimer === 0 ) {
 								        //  ...run the stop function.
 								        triviaStatus.stopTimer();
 								        //  Alert the user that time is up.
-								        alert("Time Up!");
+								        alert('Time Up!');
 								      }
 								    },
 
@@ -31,9 +47,39 @@ var triviaStatus = {startTimer:function run() {
 								      //  to the clearInterval function.
 								      clearInterval(intervalId);
 								    },
+						// checkAnswer:function capture() {
+						// 		     var playerAnswer = 
+
+						// 		    },
 						};
     //  Execute the run function.
-    triviaStatus.startTimer();
+    $('#startQuiz').on('click', function () {
+    	triviaStatus.startTimer();
+    	$(this).hide();
+    	$('#question').html(content.questions.one);
+    	//$('#answers').html('<form onSubmit="triviaStatus.captureForm()">' + content.possibleAnswers.one + '<input type="submit" value="Submit">' + '</form>');
+    	$('#answers').html('<form id=answerOptions>' + content.possibleAnswers.one + '</form>');
+	});
+    	$('#answerOptions input').on('change' function() {
+    		console.log($(this));
+  			alert($('input[type="radio"]:checked', '#answerOptions').val());
+  			
+
+  //   		var playerAnswer = $('input[name="answers"]:checked', '#answerOptions').val();
+  //   		if(playerAnswer = content.playerAnswers.one ) {
+		// 	alert("Correct Answer");	
+		// }
+		
+		// else {
+		// 	alert("Wrong Answer");
+		// }
+    	
+
+    	});
+		
+
+
+    
 
 
 
