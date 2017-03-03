7489a99d937cd4dc8f5059ae43d2f65944c9d070
caseylholston <caseylholston@gmail.com>
2017-03-03 13:31:51 -0600

    Timer is restarting, next question is added. Need to add scorekeeper and create the rest of the questions. Also need to alert the user the accuracy of their answer.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,23 +5,22 @@ $(document).ready(function(){
 var countdownTimer = 20;
 //  Variable that will hold our interval ID when we execute the "run" function
 var intervalId;
+var questionIndex =0;
+var answerIndex =0;
+var correctIndex =0;
 var numberCorrect =0;
 $('#nextQuestion').hide();
 
-var content = { questions: {one:['Who shot John F. Kennedy?'],
-							two:['What is 2+2'],
-							},
-				possibleAnswers: { one:['<ul><li><input type ="radio" name="answers" value="Rafael Cruz" id="1a"> Rafael Cruz</li>', '<li><input type ="radio"  name="answers" value="The CIA" id ="1b"> The CIA</li>', 
+var content = { questions:['Who shot John F. Kennedy?', 'What is 2+2']
+							,
+				possibleAnswers: [['<ul><li><input type ="radio" name="answers" value="Rafael Cruz" id="1a"> Rafael Cruz</li>', '<li><input type ="radio"  name="answers" value="The CIA" id ="1b"> The CIA</li>', 
 										'<li><input type ="radio" name="answers" value="Lee Harvey Oswald" id ="1c"> Lee Harvey Oswald</li>', '<li><input type ="radio" name="answers" value="Jack Ruby" id ="1d"> Jack Ruby</li></ul>'],
-						   		   two:['<input type ="radio" value="1" id="2a">1','<input type ="radio" value="1" id="2b">2',
+						   		 ['<input type ="radio" value="1" id="2a">1','<input type ="radio" value="1" id="2b">2',
 						   		   		'<input type ="radio" value="3" id="2c">3','<input type ="radio" value="4" id="2d">4'],
-								} ,
-				correctAnswers: {one:'1c',
-								 two: '2d',
-								},
-				playerAnswers: {one:'',
-							   two:'',
-							  },
+								],
+				correctAnswers: ['1c','2d'
+								],
+				playerAnswers: [],
 				};
 
 var triviaStatus = {startTimer:function run() {
@@ -50,34 +49,55 @@ var triviaStatus = {startTimer:function run() {
 								      //  to the clearInterval function.
 								      clearInterval(intervalId);
 								    },
-						// checkAnswer:function capture() {
-						// 		     var playerAnswer = 
 
-						// 		    },
+						resetTimer:function reset() {
+								      countdownTimer = 20;
+								    },
+
 						};
     //  Execute the run function.
     $('#startQuiz').on('click', function () {
     	triviaStatus.startTimer();
     	$(this).hide();
-    	$('#question').html(content.questions.one + "<br><br>");
+    	$('#question').html(content.questions[questionIndex] + "<br><br>");
     	//$('#answers').html('<form onSubmit="triviaStatus.captureForm()">' + content.possibleAnswers.one + '<input type="submit" value="Submit">' + '</form>');
-    	$('#answers').html('<form id=answerOptions>' + content.possibleAnswers.one.join('<br><br>') + '</form');
+    	$('#answers').html('<form id=answerOptions>' + content.possibleAnswers[answerIndex].join('<br><br>') + '</form');
 	});
 	$(document).on('change', 'input[type=radio][name=answers]', function(){
-
+				questionIndex++
+				answerIndex++
 			if ('input[type="radio"][name=answers]:checked')  {
 				content.playerAnswers.one = $('input[type=radio][name=answers]:checked').attr('id');
-				if( content.playerAnswers.one == content.correctAnswers.one) {
+				if( content.playerAnswers.one == content.correctAnswers[correctIndex]) {
 					console.log('you are correct');
+					triviaStatus.stopTimer();
+					triviaStatus.resetTimer();
 					numberCorrect++
 					$('#nextQuestion').show();
 				}
 				else {
 					console.log('you are wrong')
+					triviaStatus.stopTimer();
+					triviaStatus.resetTimer();
+					clearInterval(intervalId);
 					$('#nextQuestion').show();
 
 				}
-		}
+		};
+
+	
+	});
+
+
+	$('#nextQuestion').on('click', function (){
+		console.log("Next Question Function to Run");
+		$('#nextQuestion').hide();
+		
+		triviaStatus.startTimer();
+		$('#question').html(content.questions[questionIndex] + "<br><br>");
+		$('#answers').html('<form id=answerOptions>' + content.possibleAnswers[answerIndex].join('<br><br>') + '</form');
+
+
 	});
     	//if ('input[type="radio"]:checked') {}
     	
