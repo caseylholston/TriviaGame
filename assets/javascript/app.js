91c6c65e84332ef04b9378f88d62f5568f692e3c
caseylholston <caseylholston@gmail.com>
2017-03-03 22:36:31 -0600

    Need to add final score page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,16 +12,45 @@ var numberCorrect =0;
 var playerAnswerIndex = 0;
 $('#nextQuestion').hide();
 $('#scoreKeeper').empty();
+$('#correct').empty();
+$('#wrong').empty();
 //$('#scoreKeeper').html("You have answered " + numberCorrect + " questions correctly");
 
-var content = { questions:['Who shot John F. Kennedy?', 'What is 2+2']
+var content = { questions:['What NBA franchise has won the most titles?', 
+						   'Who is the all-time leading NBA scorer?',
+						   'Which NBA player has hit the most 3 pointers in one season?',
+						   'Which NBA player has won the most MVP awards?',
+						   'What is the signature move of James "The Beard" Harden?',
+						   'Who is considered the best player in the history of the NBA?',
+						   'Who composed the NBA finals theme "Roundball Rock"?',
+						   'What is the nickname of the brawl that took place between Indiana Pacer players and Detroit Pistons Fans?',
+						   'What was the name given to the Philadelphia 76ers team building philosophy that saw them lose 199 out of 246 games over three years?',
+						   'Which NBA player scored 100 points in a game?'
+							]
 							,
-				possibleAnswers: [['<ul><li><input type ="radio" name="answers" value="Rafael Cruz" id="1a"> Rafael Cruz</li>', '<li><input type ="radio"  name="answers" value="The CIA" id ="1b"> The CIA</li>', 
-										'<li><input type ="radio" name="answers" value="Lee Harvey Oswald" id ="1c"> Lee Harvey Oswald</li>', '<li><input type ="radio" name="answers" value="Jack Ruby" id ="1d"> Jack Ruby</li></ul>'],
-						   		 ['<input type ="radio" name="answers" value="1" id="2a"> 1','<input type ="radio" name="answers" value="1" id="2b"> 2',
-						   		  '<input type ="radio" name="answers" value="3" id="2c"> 3','<input type ="radio" name="answers" value="4" id="2d"> 4'],
+				possibleAnswers: [['<input type ="radio" name="answers" value="Los Angeles Lakers" id="1a"> Los Angeles Lakers', '<input type ="radio"  name="answers" value="San Antonio Spurs" id ="1b"> San Antonio Spurs', 
+								   '<input type ="radio" name="answers" value="Boston Celtics" id ="1c"> Boston Celtics', '<input type ="radio" name="answers" value="Chicago Bulls" id ="1d"> Chicago Bulls'],
+						   		 ['<input type ="radio" name="answers" value="Kobe Bryant" id="2a"> Kobe Bryant','<input type ="radio" name="answers" value="Michael Jordan" id="2b"> Michael Jordan',
+						   		  '<input type ="radio" name="answers" value="Larry Bird" id="2c"> Larry Bird','<input type ="radio" name="answers" value="Kareem Abdul-Jabbar" id="2d"> Kareem Abdul-Jabbar'],
+								 ['<input type ="radio" name="answers" value="Kobe Bryant" id="3a"> Kobe Bryant','<input type ="radio" name="answers" value="Stephen Curry" id="3b"> Stephen Curry',
+						   		  '<input type ="radio" name="answers" value="Larry Bird" id="3c"> Larry Bird','<input type ="radio" name="answers" value="Michael Jordan" id="3d"> Michael Jordan'],
+						   		 ['<input type ="radio" name="answers" value="Kareem Abdul-Jabbar" id="4a"> Kareem Abdul-Jabbar','<input type ="radio" name="answers" value="Lebron James" id="4b"> Lebron James',
+						   		  '<input type ="radio" name="answers" value="Michael Jordan id="4c"> Michael Jordan','<input type ="radio" name="answers" value="Magic Johnson" id="4d"> Magic Johnson'],
+						   		 ['<input type ="radio" name="answers" value="The Killer Crossover" id="5a"> The Killer Crossover','<input type ="radio" name="answers" value="Eurostep" id="5b"> Eurostep',
+						   		  '<input type ="radio" name="answers" value="THe Sky Hook" id="5c"> The Sky Hook','<input type ="radio" name="answers" value="The Dream Shake" id="5d"> The Dream Shake'],
+						   		 ['<input type ="radio" name="answers" value="Kobe Bryant" id="6a"> Kobe Bryant','<input type ="radio" name="answers" value="Lebron James" id="6b"> Lebron James',
+						   		  '<input type ="radio" name="answers" value="Michael Jordan" id="6c"> Michael Jordan','<input type ="radio" name="answers" value="Magic Johnson" id="6d"> Magic Johnson'],
+						   		 ['<input type ="radio" name="answers" value="John Tesh" id="7a"> John Tesh','<input type ="radio" name="answers" value="Herbie Hancock" id="7b"> Herbie Hancock',
+						   		  '<input type ="radio" name="answers" value="Quincy Jones" id="7c"> Quincy Jones','<input type ="radio" name="answers" value="Kelsey Grammer" id="7d"> Kelsey Grammer'],
+						   		 ['<input type ="radio" name="answers" value="The Pacer Paranoia" id="8a"> The Pacer Paranoia','<input type ="radio" name="answers" value="The Detroit Stampede" id="8b"> The Detroit Stampede"',
+						   		  '<input type ="radio" name="answers" value="Field of Screams" id="8c"> Field of Screams','<input type ="radio" name="answers" value="Malice at the Palace" id="8d"> Malice at the Palace'],
+						   		 ['<input type ="radio" name="answers" value="Tanks for Losing" id="9a"> Tanks for Losing','<input type ="radio" name="answers" value="The Process" id="9b"> The Process',
+						   		  '<input type ="radio" name="answers" value="You gotta lose to win" id="9c"> You gotta lose to win','<input type ="radio" name="answers" value="Lose Big to Win Big" id="9d"> Lose Big to Win Big'],
+						   		 ['<input type ="radio" name="answers" value="Kobe Bryant" id="10a"> Kobe Bryant','<input type ="radio" name="answers" value="Michael Jordan" id="10b"> Michael Jordan',
+						   		  '<input type ="radio" name="answers" value="Wilt Chamberlain" id="10c"> Wilt Chamberlain','<input type ="radio" name="answers" value="Kareem Abdul-Jabbar" id="10d"> Kareem Abdul-Jabbar']
+
 								],
-				correctAnswers: ['1c','2d'
+				correctAnswers: ['1c','2d','3b','4a','5b','6c','7a','8d','9b','10c'
 								],
 				playerAnswers: [],
 				};
@@ -75,6 +104,7 @@ var triviaStatus = {startTimer:function run() {
 				content.playerAnswers[playerAnswerIndex] = $('input[type=radio][name=answers]:checked').attr('id');
 				if( content.playerAnswers[playerAnswerIndex] == content.correctAnswers[correctIndex]) {
 					alert('You have chosen wisely');
+					$('#correct').html('<img src="../TriviaGame/assets/images/yes.gif"/>');
 					numberCorrect++;
 					console.log("Number Correct" + numberCorrect)
 					triviaStatus.stopTimer();
@@ -83,7 +113,8 @@ var triviaStatus = {startTimer:function run() {
 					$('#scoreKeeper').html("You have answered " + numberCorrect + " questions correctly");
 				}
 				else {
-					alert('You have chosen poorly');
+					alert('You have chosen poorly -- The correct answer was' + content.correctAnswers[correctIndex] );
+					$('#wrong').html('<img src="../TriviaGame/assets/images/kanyeno.gif"/>');
 					triviaStatus.stopTimer();
 					triviaStatus.resetTimer();
 					clearInterval(intervalId);
@@ -100,44 +131,14 @@ var triviaStatus = {startTimer:function run() {
 		console.log(answerIndex);
 		playerAnswerIndex++;
 		correctIndex++;
+		$('#correct').empty();
+		$('#wrong').empty();
 		$('#nextQuestion').hide();
 		triviaStatus.startTimer();
 		$('#question').html(content.questions[questionIndex] + "<br><br>");
 		$('#answers').html('<form id=answerOptions>' + content.possibleAnswers[answerIndex].join('<br><br>') + '</form');
 
 	});
-
-    	//if ('input[type="radio"]:checked') {}
-    	
-    // 	$('input[type=radio][name=answers]').change(function() {
-  		// 	console.log("I was pressed");
-  		// 	//alert($('input[name="answers"]:checked').val());
-  		// });
-
-  //   		var playerAnswer = $('input[name="answers"]:checked', '#answerOptions').val();
-  //   		if(playerAnswer = content.playerAnswers.one ) {
-		// 	alert("Correct Answer");	
-		// }
-		
-		// else {
-		// 	alert("Wrong Answer");
-		// }
-    	
-
-    
-		
-
-
-    
-
-
-
-
-
-
-
-
-
 
 
 
